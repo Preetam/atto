ea950916da90af6bf9d288e5c9764523b776cb86
Preetam Jinka <pj@isomero.us>
2012-12-24 17:43:09 -0500

    Fixed instancing issue.

--- index.js
+++ index.js
@@ -46,23 +46,23 @@ var atto = function (memcachedPort, memcachedHost, dbTarget, bucketName, usr, pw
 	this.readyEmitter = new (require('events')).EventEmitter();
 
 	this.nodes = null;
-	memcached = new memcached(memcachedHost+':'+memcachedPort);
+	this.memcached = new memcached(memcachedHost+':'+memcachedPort);
 
 	this.set = function (key, value, cb, lifetime) {
 		if(!lifetime) lifetime = 0;
-		memcached.set(key, value, lifetime, cb);
+		this.memcached.set(key, value, lifetime, cb);
 	};
 
 	this.get = function (key, cb) {
-		memcached.get(key, cb);
+		this.memcached.get(key, cb);
 	};
 
 	this.incr = function (key, value, cb) {
-		memcached.increment(key, value, cb);
+		this.memcached.increment(key, value, cb);
 	};
 
 	this.decr = function (key, value, cb) {
-		memcached.decrement(key, value, cb);
+		this.memcached.decrement(key, value, cb);
 	};
 
 	this.view = function (designDoc, viewName, params, cb) {
